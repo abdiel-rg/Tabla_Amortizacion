aa5a3dc324c8fa266243b69d8c0116d167a413af
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-03-19 15:33:38 -0400

    Update TablaAmortizacion.cs
    Add constructors and Pagos method

--- Data/TablaAmortizacion.cs
+++ Data/TablaAmortizacion.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tabla_Amortizacion.Data;
 
 namespace Tabla_Amortizacion.Pages
 {
     public partial class TablaAmortizacion
     {
+        public TablaAmortizacion(double Monto, double TasaAnual, int PlazoAnios)
+        {
+            this.Monto = Monto;
+            this.TasaAnual = TasaAnual;
+            this.PlazoAnios = PlazoAnios;
+        }
+
+        public TablaAmortizacion() { }
+
         public double Monto { get; set; }
         public double TasaAnual { get; set; }
         public int PlazoAnios { get; set; }
@@ -15,5 +25,22 @@ namespace Tabla_Amortizacion.Pages
         public int PlazoMeses => PlazoAnios * 12;
 
         public double CuotaProg => Monto * (TasaMensual * Math.Pow(1 + TasaMensual, PlazoMeses) / (Math.Pow(1 + TasaMensual, PlazoMeses) - 1));
+
+        public IEnumerable<Pago> Pagos()
+        {
+            Pago pago = new() { BalanceInicial = Monto };
+
+            for (int i = 1; i <= PlazoMeses; i++)
+            {
+                pago.Numero = i;
+                pago.Intereses = pago.BalanceInicial * TasaMensual;
+                pago.AbonoCapital = CuotaProg - pago.Intereses;
+                pago.BalanceFinal = pago.BalanceInicial - pago.AbonoCapital;
+
+                yield return pago;
+
+                pago.BalanceInicial = pago.BalanceFinal;
+            }
+        }
     }
 }
