9612c4098cc4970a4ac96ad4f235e54d03bf78b2
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-03-19 15:23:23 -0400

    Add TablaAmortizacion class

---
+++ Data/TablaAmortizacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabla_Amortizacion.Pages
+{
+    public partial class TablaAmortizacion
+    {
+        public double Monto { get; set; }
+        public double TasaAnual { get; set; }
+        public int PlazoAnios { get; set; }
+
+        public double TasaMensual => TasaAnual / 100 / 12;
+        public int PlazoMeses => PlazoAnios * 12;
+
+        public double CuotaProg => Monto * (TasaMensual * Math.Pow(1 + TasaMensual, PlazoMeses) / (Math.Pow(1 + TasaMensual, PlazoMeses) - 1));
+    }
+}
