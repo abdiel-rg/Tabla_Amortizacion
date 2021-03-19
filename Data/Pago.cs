f336fae6e56754614ff8f6280176dd7cbc4d7f56
abdiel-rg <54369065+abdiel-rg@users.noreply.github.com>
2021-03-19 15:24:40 -0400

    Add Pago class

---
+++ Data/Pago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabla_Amortizacion.Data
+{
+    public class Pago
+    {
+        public int Numero { get; set; }
+
+        public double BalanceInicial { get; set; }
+
+        public double Intereses { get; set; }
+
+        public double AbonoCapital { get; set; }
+
+        public double BalanceFinal { get; set; }
+    }
+}
